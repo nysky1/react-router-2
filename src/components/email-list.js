f84a080aadf8d532e5b3a0803c3b6ecba13c383f
J. Bishop <jbishop@Js-MacBook-Pro.local>
2018-08-06 08:04:32 -0600

    init

---
+++ src/components/email-list.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+import './email-list.css';
+
+export function EmailList(props) {
+    const emails = props.emailList.map(email =>
+        <li key={email.id} className="email-list-email">
+            <div className="email-list-email-from">
+                {email.from}
+            </div>
+            <div className="email-list-email-title">
+                <Link to={`${props.folderId}/${email.id}`} >{email.title}</Link>
+            </div>
+        </li>
+    );
+
+    return (
+        <div className="folder">
+            <h2>{props.folderName}</h2>
+            <ul className="email-list">
+                {emails}
+            </ul>
+        </div>
+    );
+}
+
+const mapStateToProps = (state, props) => {
+
+    const folderId = props.match.params.folderId; 
+
+    const folder = state[folderId];
+
+    return {
+        folderId,
+        folderName: folder.name,
+        emailList: Object.keys(folder.emails).map(emailId =>
+            folder.emails[emailId]
+        )
+    }
+};
+
+export default connect(mapStateToProps)(EmailList);
---
+++ src/components/email.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Sidebar from './sidebar';
+import {BrowserRouter as Router, Route , Link} from 'react-router-dom';
+
+import SingleEmail from './single-email';
+import EmailList from './email-list';
+import './email.css';
+
+export default function Email() {
+    return (
+        <Router>
+        <div className="email">
+            <Sidebar />
+            <main>
+                <Route exact path="/:folderId" component={EmailList} />
+                <Route exact path="/:folderId/:emailId" component={SingleEmail} />
+            </main>
+        </div>
+        </Router>
+    );  
+}
